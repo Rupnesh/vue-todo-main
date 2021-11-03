1ab18fcdcaec1b14f79053dc8a8cc1f404d22082
Rupnesh <sharmarupnesh@gmail.com>
2021-11-03 12:55:10 +0530

    vue store

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -2,43 +2,31 @@
   <div class="container">
   <TaskProgress v-bind:todo="countSpecificTask('to-do')" v-bind:progress="countSpecificTask('in-progress')" v-bind:finished="countSpecificTask('finished')"></TaskProgress>
   <CreateTask @addTask="handleAddTask"></CreateTask>
-  <TaskList v-bind:tasks="tasks"></TaskList>
+  <TaskList></TaskList>
     
   </div>
 </template>
 
 <script>
+import store from '../store';
+import * as type from '../storeTypes';
 import TaskProgress from './TaskProgress.vue';
 import CreateTask from './CreateTask.vue';
 import TaskList from './TaskList.vue';
+import { mapState } from 'vuex';
+
 export default {
   name: 'ToDo',
-  props: {
-    msg: String
-  },
   components: {
     TaskProgress, CreateTask, TaskList
   },
+  computed: mapState({
+    tasks: state => state.tasks
+  }),
   mounted() {
-    this.countTask();
-  },
-  data() {
-    return {
-      tasks: [ 
-        {task: "task 1", status: "to-do", edit: false},
-        {task: "task 2", status: "to-do", edit: false},
-        {task: "task 3", status: "in-progress", edit: false},
-        {task: "task 4", status: "to-do", edit: false},
-        {task: "task 5", status: "finished", edit: false},
-      ],
-    }
   },
 
   methods : {
-    
-    countTask() {
-      this.$emit('showTasksCount', this.tasks.length);
-    },
     countSpecificTask(task) {
       return this.tasks.reduce((previousValue, currentObject) => {
           return previousValue + (currentObject.status === task); 
@@ -46,8 +34,7 @@ export default {
     },
 
     handleAddTask(task) {
-      this.tasks.push(task)
-      this.countTask()
+      store.dispatch({ type: type.Add, data: task })
     }
   }
 }
